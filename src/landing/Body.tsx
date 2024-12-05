42362b27a458f4aba5b038819cde4ea839ef6d36
Arnab Das <93949960+Arnab7456@users.noreply.github.com>
2024-12-06 01:46:36 +0530

    Update Body.tsx

--- src/landing/Body.tsx
+++ src/landing/Body.tsx
@@ -8,7 +8,7 @@ export default function Body() {
         <div className="flex flex-col justify-center items-center h-[35rem] px-4">
             <div className="text-4xl font-normal text-neutral-600 dark:text-neutral-400 text-center">
                 Welcome to <FlipWords words={words} /> <br />
-                ChatApp  using for seamless communication
+                Chat App  used for seamless communication
             </div>
             <Link to="/chat">
                 <button className="mt-8 text-xl font-sans font-medium bg-blue-600 px-20 py-3 rounded-lg text-white hover:bg-blue-800   transition">
